9785e4c1884a711af1a9eeee150f633fd67517ba
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2026-05-08 21:49:54 +0530

    Product entity updated

--- backend/product-service/src/main/java/com/rinos/productservice/entity/Product.java
+++ backend/product-service/src/main/java/com/rinos/productservice/entity/Product.java
@@ -1,16 +1,18 @@
 package com.rinos.productservice.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "products")
-@Getter
-@Setter
-public class Products {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -20,7 +22,12 @@ public class Products {
     private double price;
     private String brand;
     private int categoryId;
-    private String Status;
+    private String status;
+
+    @CreationTimestamp  // Auto-sets on create
+    @Column(updatable = false)  // Never update after creation
     private Date createdAt;
+
+    @UpdateTimestamp  // Auto-sets on create & update
     private Date updatedAt;
 }
