2045dd768f53b8ff99a58cf5a27fdbffb0c9d5f4
ramlanrinos <e21336@eng.pdn.ac.lk>
2026-07-27 21:44:50 +0530

    product service created

---
+++ backend/product-service/src/test/java/org/rinos/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rinos.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
